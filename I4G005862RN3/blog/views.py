b60eb7f11b6a42439f15e5d46be42ed055034d4a
arabog <arababs@gmail.com>
2022-06-21 17:15:38 +0100

    commit   2

--- I4G005862RN3/blog/views.py
+++ I4G005862RN3/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+from .models import Post
+
+
+
+class PostListView(generic.ListView):
+    model = Post
+
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+
+
+class PostDetailView:
+    model = Post
